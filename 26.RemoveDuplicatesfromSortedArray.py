f8da9195eef68b319195f455506d9baf31e177dc
yuanwb1984 <yuan.wb@gmail.com>
2016-06-22 11:44:29 +0800

    26.Remove Duplicates from Sorted Array

---
+++ 26.RemoveDuplicatesfromSortedArray.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jun 22 11:20:40 2016
+
+@author: 06210
+查找字典是否有，有pop掉，无添加到字典。
+"""
+
+class Solution(object):
+    def removeDuplicates(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        pool = {}
+        l = len(nums) -1
+        for i in range(l, -1, -1):
+            if nums[i] in pool:
+                nums.pop(i)
+            else:
+                pool[nums[i]] = i
+        return len(nums)
+n = Solution()
+print(n.removeDuplicates([1,1,2,1,2]))
